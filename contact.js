b3e1a4b41c412ff6b564d5925e62b1f22b14c2d8
kavithajajula <kavithaj@pdx.edu>
2023-05-26 12:10:06 -0700

    Contact page files submission

---
+++ contact.js
@@ -0,0 +1,18 @@
+let form = document.querySelector("form");
+
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  alert("Form has been submitted successfully.");
+
+  let userName = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let subject = document.getElementById("subject").value;
+  let message = document.getElementById("message").value;
+
+  console.log(form);
+  console.log("=================Form Submission==============");
+  console.log("Name:", userName);
+  console.log("Email:", email);
+  console.log("Subject:", subject);
+  console.log("Message:", message);
+}
